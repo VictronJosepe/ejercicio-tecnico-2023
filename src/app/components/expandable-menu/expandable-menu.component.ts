08daf47196eb7ce35a761be5fd90c58e57f3bcba
Victor <vjose_2007@hotmail.com>
2023-12-30 21:30:48 -0500

    VSI - Se completa la funcionalidad de borrado de producto

--- src/app/components/expandable-menu/expandable-menu.component.ts
+++ src/app/components/expandable-menu/expandable-menu.component.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { IProduct } from 'src/app/interfaces/IProduct';
+import { HttpRequestsService } from 'src/app/services/httpRequests/http-requests.service';
 
 @Component({
   selector: 'app-expandable-menu',
@@ -10,15 +12,23 @@ import { IProduct } from 'src/app/interfaces/IProduct';
 export class ExpandableMenuComponent {
   @Input() product!: IProduct;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpRequestsService) { }
 
   protected navigateEditProduct() {
     sessionStorage.setItem("productInfo", JSON.stringify(this.product));
     this.router.navigateByUrl('/ejercicio/formulario');
   }
 
-  protected deleteProduct(id: string) {
-    console.log("id", id);
-    // this.router.navigateByUrl('/ejercicio/products');
+  protected deleteProduct(id: string, name: string) {
+    if (window.confirm('¿Estás seguro de eliminar el producto ' + name + '?')) {
+      this.http.deleteProduct(id).subscribe({
+        next: (resp: IProduct[]) => {
+          window.location.reload();
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error(error.message);
+        }
+      });
+    }
   }
 }
